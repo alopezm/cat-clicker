cebe2c6faf2d6e6c8c092dfa7ae1b003b6a36e49
Anderson LM <anderson1564@gmail.com>
2016-03-26 18:02:53 -0500

    quiz 3

--- cat-clicker.js
+++ cat-clicker.js
@@ -1,4 +1,27 @@
 (function (window, document, undefined) {
+
+    var mapNames = {
+        'Romeo' : 'media/cat0.jpg',
+        'Lucho' : 'media/cat1.jpg',
+        'Mandy' : 'media/cat2.jpg',
+        'Billy' : 'media/comfortable-cat.jpg',
+        'Shamizen' : 'media/cat-1404368.jpg'
+    };
+
+    function displayCat(event)
+    {
+        var target = event.target;
+        if(target.tagName.toLowerCase() === 'li') {
+            var name = target.innerText;
+            var src = mapNames[name];
+
+            document.getElementById('name').innerHTML = name;
+            document.getElementById('cat').setAttribute('src', src);
+            // reset the counter
+            document.getElementById('counter').setAttribute('value', '0');
+        }
+    }
+
     function addCounter()
     {
         var counter = document.getElementById('counter');
@@ -6,7 +29,11 @@
         counter.setAttribute('value', newValue);
     }
 
+    var names = document.getElementById("names");
     var cats = document.getElementsByClassName("cat");
+    
+    names.addEventListener("click", displayCat);
+
     for(var i = 0; i < cats.length; ++i) {
         cats[i].addEventListener("click", addCounter);
     }
