0b770058a8b91ed5d7255d4252acb6810ca1a6b3
Anderson LM <anderson1564@gmail.com>
2016-03-28 12:27:59 -0500

    Optmization on the views and models

--- cat-clicker.js
+++ cat-clicker.js
@@ -2,98 +2,110 @@
 
     var model = {
         init: function() {
-            this.initCats();
-            this.setCurrentCat(this.getFirstCat());
-        },
-        initCats: function () {
-            var cats = {
-                'Romeo' : {img: 'media/cat0.jpg', count: 0},
-                'Lucho' : {img: 'media/cat1.jpg', count: 0},
-                'Mandy' : {img: 'media/cat2.jpg', count: 0},
-                'Billy' : {img: 'media/cat3.jpg', count: 0},
-                'Shamizen' : {img: 'media/cat-1404368.jpg', count: 0}
-            };
-
-            for(var name in cats) {
-                cats[name].name = name;
-            }
-            this.cats = cats;
+            this.cats = [
+                {img: 'media/cat0.jpg', count: 0, name: 'Romeo'},
+                {img: 'media/cat1.jpg', count: 0, name: 'Lucho'},
+                {img: 'media/cat2.jpg', count: 0, name: 'Mandy'},
+                {img: 'media/cat3.jpg', count: 0, name: 'Billy'},
+                {img: 'media/cat5.jpg', count: 0, name: 'Shamizen'}
+            ];
+            this.saveCurrentCat(this.cats[0]);
         },
         getCats: function () {
             return this.cats;
         },
         getCat: function (name) {
-            var cats = this.getCats();
-            return cats[name];
-        },
-        getFirstCat: function() {
-            var names = this.getNames();
-            return names[0];
+            var selectCat = undefined;
+            var filterCats = this.cats.filter(function (cat) {
+                return cat.name === name;
+            });
+            if(filterCats.length >= 1) {
+                selectCat = filterCats[0];
+            }
+            else {
+                console.error('Error with the current cat');
+            }
+            return selectCat;
         },
         setCurrentCat: function(name) {
-            this.currentCat = name;
+            this.currentCat = this.getCat(name);
+        },
+        saveCurrentCat: function (currentCat) {
+            this.setCat(currentCat);
+            this.currentCat = currentCat;
         },
         getCurrentCat: function() {
-            return this.getCat(this.currentCat);
+            return this.getCat(this.currentCat.name);
         },
-        setCat: function (cat) {
-            this.cats[cat.name] = cat;
+        setCat: function (newCat) {
+            // remove duplicates by name
+            var cats = this.cats.filter(function (oldCat) {
+                return oldCat.name !== newCat.name;
+            });
+            // Insert new cat
+            cats.push(newCat);
+            this.cats = cats;
         },
         updateCounterCurrentCat: function () {
-            var currentCat = this.getCurrentCat();
-            currentCat.count++;
-            this.setCat(currentCat);
+            this.currentCat.count++;
+            this.saveCurrentCat(this.currentCat);
         },
         getNames: function () {
-            return Object.keys(this.getCats());
+            return this.cats.map(function (cat) {
+                return cat.name;
+            })
         }
     };
-    
-    var view = {
+
+    var listCats = {
         init: function () {
-            this.initCatSelector();
-            this.displayCat();
-
-            var cat = document.getElementById("cat");
-            cat.addEventListener("click", this.bindCounter);
-
+            this.listCatsElement = document.getElementById('list-cats');
+            this.render();
+            this.bindElements();
         },
-        bindCounter: function ()
-        {
-            var counter = document.getElementById('counter');
-            var currentCat = octopus.getCurrentCat();
-            octopus.updateCounterCurrentCat();
-            counter.setAttribute('value', currentCat.count);
+        bindElements: function () {
+            this.listCatsElement.addEventListener("click", function (event) {
+                var target = event.target;
+                if(target.tagName.toLowerCase() === 'li') {
+                    var name = target.innerText;
+                    octopus.setCurrentCat(name);
+                }
+            });
         },
-        displayCat: function () {
-            var cat = octopus.getCurrentCat();
-            document.getElementById('name').innerHTML = cat.name;
-            document.getElementById('cat').setAttribute('src', cat.img);
-            document.getElementById('counter').setAttribute('value', cat.count);
-        },
-        initCatSelector: function () {
-            var namesElem = document.getElementById('names');
+        render: function () {
             var names = octopus.getNames();
             var htmlStr = '';
             names.forEach(function(name) {
                 htmlStr += '<li>'+name+'</li>';
             });
-            namesElem.innerHTML = htmlStr;
+            this.listCatsElement.innerHTML = htmlStr;
+        }
+    };
 
-            namesElem.addEventListener("click", function (event) {
-                var target = event.target;
-                if(target.tagName.toLowerCase() === 'li') {
-                    var name = target.innerText;
-                    octopus.setCurrentCat(name);
-                    view.displayCat();
-                }
-            });
+    var view = {
+        init: function () {
+            this.catImgElement = document.getElementById("cat-img");
+            this.catNameElement = document.getElementById('cat-name');
+            this.catCounterElement = document.getElementById('cat-counter');
+
+            this.render();
+            this.catImgElement.addEventListener("click", this.bindCounter);
+        },
+        bindCounter: function () {
+            octopus.updateCounterCurrentCat();
+        },
+        render: function () {
+            var currentCat = octopus.getCurrentCat();
+            this.catNameElement.innerHTML = currentCat.name;
+            this.catImgElement.setAttribute('src', currentCat.img);
+            this.catCounterElement.setAttribute('value', currentCat.count);
         }
     };
 
     var octopus = {
         init: function () {
             model.init();
+            listCats.init();
             view.init();
         },
         getCat: function (name) {
@@ -107,9 +119,11 @@
         },
         setCurrentCat: function (name) {
             model.setCurrentCat(name);
+            view.render();
         },
         updateCounterCurrentCat: function () {
             model.updateCounterCurrentCat();
+            view.render();
         },
         getNames: function () {
             return model.getNames();
